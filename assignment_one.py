f363c4183bc60b933ac5a69667fb4ad2fb7bdc2a
owen28445 <Fengji@ssfs.org>
2022-10-26 09:37:21 -0400

    final project

--- assignment_one.py
+++ assignment_one.py
@@ -1,1 +1,70 @@
-# remember to use comments!
+# Owen Feng
+# 10-22-22
+import turtle
+
+def draw_octagons(side_length, color):
+    '''this fuction draw a octagons'''
+    turtle.fillcolor(color)
+    turtle.begin_fill()
+
+    for x in range(8):
+        turtle.forward(90)
+        turtle.left(45)
+
+    turtle.end_fill()
+
+# calls the function
+draw_octagons(100,"red")
+
+
+#moves the pen
+turtle.penup()
+turtle.goto(-280,90)
+turtle.pendown()
+
+'''draw a second octagons'''
+
+draw_octagons(100,"blue")
+
+turtle.begin_fill()
+
+
+
+turtle.end_fill()
+
+
+turtle.penup()
+turtle.goto(-230,-300)
+turtle.pendown()
+
+'''draw a third octagons'''
+
+draw_octagons(100,"pink")
+
+
+turtle.begin_fill()
+
+
+turtle.end_fill()
+
+
+turtle.penup()
+turtle.goto(120,-300)
+turtle.pendown()
+
+'''draw a forth octagons'''
+
+draw_octagons(100,"green")
+
+
+turtle.end_fill()
+
+
+
+
+
+
+turtle.exitonclick()
+
+
+
